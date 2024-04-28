7b61e5b43527bfb23e4c67003327040d701f0af7
mydatahack <thon6thon6@hotmail.com>
2024-04-28 10:34:34 +1000

    Add jest tests

--- apps/react-my-playground/jest.config.ts
+++ apps/react-my-playground/jest.config.ts
@@ -3,5 +3,6 @@
 export default {
   displayName: 'react-my-playground',
   preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   coverageDirectory: '../../coverage/apps/react-my-playground',
 };
--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,16 @@
 const nxPreset = require('@nx/jest/preset').default;
 
-module.exports = { ...nxPreset };
+module.exports = {
+  ...nxPreset,
+  // https://stackoverflow.com/questions/74878470/nx-and-jest-dont-find-all-my-tests-that-end-with-spec-tsx
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'mjs', 'html'],
+  transform: {
+    '^.+\\.(tsx?|js|html)$': [
+      'ts-jest',
+      {
+        //apps/react-my-playground/tsconfig.spec.json
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+};
