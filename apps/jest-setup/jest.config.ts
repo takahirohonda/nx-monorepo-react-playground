f73a2056fc05f09a3a088fad51a9133d1909d366
mydatahack <thon6thon6@hotmail.com>
2024-05-08 13:52:25 +1000

    Remove unncessary apps

--- apps/jest-setup/jest.config.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable */
-export default {
-  displayName: 'jest-setup',
-  preset: '../../jest.preset.js',
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-  coverageDirectory: '../../coverage/apps/jest-setup',
-};
