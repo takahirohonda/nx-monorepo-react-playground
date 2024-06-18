31b18a9449af8a02b90efef46048a5de21b6d88f
takahirohonda <thon6thon6@hotmail.com>
2024-06-18 23:45:52 +1000

    Add new table

--- apps/next-mood/src/entity/User.ts
+++ apps/next-mood/src/entity/User.ts
@@ -1,16 +1,99 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BaseEntity,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToMany,
+  ManyToOne,
+  OneToOne,
+  JoinColumn,
+} from 'typeorm'
 
 @Entity()
-export class User {
-  @PrimaryGeneratedColumn()
-  id!: number
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @CreateDateColumn()
+  createdAt: Date
+
+  @UpdateDateColumn()
+  updatedAt: Date
+
+  @Column({ unique: true })
+  clerkId: string
+
+  @Column({ unique: true })
+  email: string
+
+  @OneToMany(() => JournalEntry, (journalEntry) => journalEntry.user)
+  journalEntries: JournalEntry[]
+}
+
+@Entity()
+export class JournalEntry extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @CreateDateColumn()
+  createdAt: Date
+
+  @UpdateDateColumn()
+  updatedAt: Date
 
   @Column()
-  firstName!: string
+  userId: string
+
+  @ManyToOne(() => User, (user) => user.journalEntries)
+  user: User
+
+  @Column('text')
+  content: string
+
+  @Column({ nullable: true })
+  analysisId: string
+
+  @ManyToOne(() => Analysis, (analysis) => analysis.entry, { nullable: true })
+  analysis: Analysis
+
+  // Optional index
+  // @Index()
+  // userId: string;
+}
+
+@Entity()
+export class Analysis extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @CreateDateColumn()
+  createdAt: Date
+
+  @UpdateDateColumn()
+  updatedAt: Date
 
   @Column()
-  lastName!: string
+  entryId: string
+
+  @OneToOne(() => JournalEntry, (journalEntry) => journalEntry.analysis)
+  @JoinColumn()
+  entry: JournalEntry
 
   @Column()
-  age!: number
+  mood: string
+
+  @Column('text')
+  summary: string
+
+  @Column()
+  color: string
+
+  @Column()
+  negative: boolean
+
+  // Unique constraint
+  // @Unique()
+  // entryId: string;
 }
