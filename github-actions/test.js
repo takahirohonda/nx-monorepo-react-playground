775d443f14e8cee6a6e2c704f369801f8bdbbfda
takahirohonda <thon6thon6@hotmail.com>
2024-06-23 14:06:16 +1000

    Update test.js

--- github-actions/test.js
+++ github-actions/test.js
@@ -1,1 +1,8 @@
-console.log('Hello from my custom action...')
+const core = require('@actions/core')
+
+try {
+  const message = core.getInput('message')
+  console.log(`Message: ${message}`)
+} catch (error) {
+  core.setFailed(error.message)
+}
