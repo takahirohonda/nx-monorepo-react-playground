e3fc77bf240223a9de1ab36dfc11cb6920e43eea
takahirohonda <thon6thon6@hotmail.com>
2024-08-27 13:19:55 +0200

    Add more phrases

--- apps/learn-german/src/app/const/conversations.ts
+++ apps/learn-german/src/app/const/conversations.ts
@@ -24,3 +24,56 @@ export const TAXI = [
   'Halten Sie bitte hier.',
   'Ich steige hier aus.',
 ]
+
+export const ZUG = [
+  'Eine Fahrkarte nach Heidelberg, bitte.',
+  'Wie lange dauert es mit dem Zug?',
+  'Wie lange dauert es mit der U-Bahn?',
+  'Wie lange dauert es mit dem Intercity?',
+  'Zwei Fahrkarten von Augsburg nach München, bitte.',
+  'Wann fährt der Zug nach Berlin ab?',
+  'Wann kommen wir in Hamburg an?',
+]
+
+export const REZEPTION = [
+  'Ich habe eine Reservierng für zwei Nächte.',
+  'Ich möchte ein Zimmer mit Dusche.',
+  'Ich möchte ein Einzelzimmer mit Bad.',
+  'Ich möchte ein Doppelzimmer.',
+  'Ich möchte ein Zimmer mit zwei Betten.',
+  'Ich möchte ein Zimmer mit schöner Aussicht.',
+  'Ich habe keine Reservierung.',
+  'Was kostet es pro Nacht?',
+  'Ist das Frühstück inbegriffen?',
+]
+
+export const HOTELZIMMER = [
+  'Es gibt keinen Fön.',
+  'Bringen Sie mir bitte eine Decke.',
+  'Bringen Sie mir bitte eine Flashe Weißwein.',
+  'Die Klimaanlage funktioniert nicht.',
+  'Die Dusche funktioniert nicht.',
+  'Ich möchte das Frühstück aufs Zimmer bestellen.',
+  'Kann ich ein Extrabett im Zimmer haben?',
+]
+
+export const MARKT = [
+  'Ich suche ein Geschäft mit günstigen Preisen.',
+  'Wo kann man das kaufen?',
+  'Wo kann man Obst kaufen?',
+  'Wo kann man Käse kaufen?',
+  'Wo kann man Wurst kaufen?',
+  'Wo kann man Kuchen kaufen?',
+  'Was sind die Spezialitäten dieser Stadt?',
+  'Ich suche ein Geschenk für meine Schwester.',
+  'Ich Hätte gern ein Kilo Kartoffeln.',
+  'Drei Sück Apfelkuchen, bitte.',
+]
+
+export const KAUFHAUS = [
+  'Wo ist die Bekleidungsabteilung?',
+  'Wo ist die Haushaltswarenabteilung?',
+  'Wo ist die Schmuckwarenabteilung?',
+  'Wo ist die Schreibwarenabteilung?',
+  'Wo ist die Spielzeugabteilung?',
+]
--- apps/learn-german/src/app/conversations.ts
+++ apps/learn-german/src/app/conversations.ts
@@ -1,7 +1,20 @@
 import '../index.css'
-import { FLUGZEUG, TAXI } from './const/conversations'
+import {
+  FLUGZEUG,
+  TAXI,
+  ZUG,
+  REZEPTION,
+  HOTELZIMMER,
+  MARKT,
+  KAUFHAUS,
+} from './const/conversations'
 import { createSentenceList } from './utils'
 ;(function () {
   createSentenceList([...FLUGZEUG], 'flugzeug')
   createSentenceList([...TAXI], 'taxi')
+  createSentenceList([...ZUG], 'zug')
+  createSentenceList([...REZEPTION], 'rezeption')
+  createSentenceList([...HOTELZIMMER], 'hotelzimmer')
+  createSentenceList([...MARKT], 'markt')
+  createSentenceList([...KAUFHAUS], 'kaufhaus')
 })()
