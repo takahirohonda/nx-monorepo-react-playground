48ff5249e749d4b6d0cb632da0408d79861f6a38
takahirohonda <thon6thon6@hotmail.com>
2024-08-27 12:03:50 +0200

    Update

--- apps/learn-german/src/app/conversations.ts
+++ apps/learn-german/src/app/conversations.ts
@@ -1,6 +1,7 @@
 import '../index.css'
-import { FLUGZEUG } from './const/conversations'
+import { FLUGZEUG, TAXI } from './const/conversations'
 import { createSentenceList } from './utils'
 ;(function () {
   createSentenceList([...FLUGZEUG], 'flugzeug')
+  createSentenceList([...TAXI], 'taxi')
 })()
