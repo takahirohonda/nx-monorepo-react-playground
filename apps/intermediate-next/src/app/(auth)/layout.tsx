81d11a76d00e59520dad5a522cb25fff92a43364
takahirohonda <thon6thon6@hotmail.com>
2024-12-16 17:42:12 +1100

    chore: small type fix

--- apps/intermediate-next/src/app/(auth)/layout.tsx
+++ apps/intermediate-next/src/app/(auth)/layout.tsx
@@ -1,4 +1,4 @@
-const AuthLayout = ({ children }) => {
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
       <div className="w-full max-w-[400px] mx-auto">{children}</div>
